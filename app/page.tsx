a6e089a501807cdfb89903a9f275f97dbcb40b52
Kanishk <kanishkkumar127@gmail.com>
2024-11-23 13:01:58 +0530

    OTP authentication Up and running

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl text-brand-100">Store It</h1>
+    </div>
+  );
+}
